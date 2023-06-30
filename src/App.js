fbb906a1672a61b20ad1df037641367c950e1248
Marouane Dhambri <marouane.dhambri@agence-inspire.com>
2023-06-30 14:52:52 +0100

    first time

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import pics from './db.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 9; // Number of pictures per page
+
+  // Calculate the start and end indices for the current page
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  // Get the pictures for the current page
+  const currentPictures = pics.engagement.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(pics.engagement.length / pageSize);
+
+  const handlePageChange = (page) => {
+    setLoading(true);
+    setCurrentPage(page);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    handlePageChange(1);
+  }, []);
+
+  return loading ? (
+    <span>LOADING . . .</span>
+  ) : (
+    <div className='App'>
+      {currentPictures.map((pic, index) => (
+        <img className='onePic' src={pic.path} alt={index} key={index} />
+      ))}
+      <div className='pagination'>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (page) => (
+            <button
+              key={page}
+              className={currentPage === page ? 'button active' : 'button'}
+              onClick={() => handlePageChange(page)}
+            >
+              {page}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 }
